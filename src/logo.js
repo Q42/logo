8376018a6f8059b0f8d4e48bac5d69f25107bb17
Marcel Duin <marcel@q42.nl>
2016-03-29 12:36:45 +0200

    spaces to tabs

--- src/logo.js
+++ src/logo.js
@@ -1,39 +1,44 @@
 // Logo main constructor
 function Q42Logo(element){
-  console.info('New logo!', element);
+	console.info('New logo!', element);
 
-  this.element = element;
+	this.element = element;
 
-  this.rendererName = element['dataset']['renderType'] || 'SVG';
-  this.renderer = Q42Logo[this.rendererName] && new Q42Logo[this.rendererName](this) || new Q42Logo['SVG'](this);
+	this.rendererName = element['dataset']['renderType'] || 'SVG';
+	this.renderer = Q42Logo[this.rendererName] && new Q42Logo[this.rendererName](this) || new Q42Logo['SVG'](this);
 
-  // prototype bindings to instance
-  this.setSize = this.setSize.bind(this);
-  this.setSizeDeferred = this.setSizeDeferred.bind(this);
+	// prototype bindings to instance
+	this.setSize = this.setSize.bind(this);
+	this.setSizeDeferred = this.setSizeDeferred.bind(this);
 
-  // internals
-  this._setSizeAf = null;
+	// internals
+	this._setSizeAf = null;
 
-  this.init();
+	this.init();
 };
 
 Q42Logo.prototype = {
-  init: function(){
-    this.renderer.init && this.renderer.init();
+	init: function(){
+		this.renderer.init && this.renderer.init();
 
-    addEventListener('resize', this.setSize);
-    this.setSize();
-  },
+		addEventListener('resize', this.setSize);
+		this.setSize();
+	},
 
-  // Set (dynamic) width/height
-  setSize: function(){
-    cancelAnimationFrame(this._setSizeAf);
-    this._setSizeAf = requestAnimationFrame(this.setSizeDeferred);
-  },
-  setSizeDeferred: function(){
-    if(this.renderer.setSize)
-      this.renderer.setSize();
-  }
+	// Set (dynamic) width/height
+	setSize: function(){
+		cancelAnimationFrame(this._setSizeAf);
+		this._setSizeAf = requestAnimationFrame(this.setSizeDeferred);
+	},
+	setSizeDeferred: function(){
+		if(this.renderer.element) {
+			this.renderer.element.style.width = this.element.offsetWidth + 'px';
+			this.renderer.element.style.height = this.element.offsetHeight + 'px';
+		}
+
+		if(this.renderer.setSize)
+			this.renderer.setSize();
+	}
 };
 
 // For minification
