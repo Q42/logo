eb891d0abb79ba035c865f75dc2812adbc04a289
Marcel Duin <marcel@q42.nl>
2016-04-01 13:33:25 +0200

    webgl-freak has both fragment and shader example

--- src/modules/webgl-freak.js
+++ src/modules/webgl-freak.js
@@ -2,12 +2,26 @@ Q42Logo['webgl-freak'] = function(logo){
 	this.logo = logo;
 };
 
-Q42Logo['webgl-freak'].prototype = Object.create(Q42Logo.WebGL.prototype);
-Q42Logo['webgl-freak'].prototype.vertexShader = [
+var proto = Q42Logo['webgl-freak'].prototype = Object.create(Q42Logo.WebGL.prototype);
+proto.vertexShader = [
 	"attribute vec2 pos;",
 	"uniform float time;",
+	"uniform vec2 mousePos;",
+	"varying vec2 mousePosF;",
 	"void main()",
 	"{",
-			"gl_Position = vec4(pos.x*sin((pos.y*time)/200.),pos.y*cos(pos.x*time/1000.),pos.y*cos(time/2000.),1.0);",
+		"mousePosF = mousePos;",
+		"gl_Position = vec4(pos.x*mousePos.x+pos.x*sin((pos.y*time)/200.),mousePos.y*pos.y+pos.y*cos(pos.x*time/1000.),pos.y*cos(time/2000.),1.0);",
+	"}"
+].join("\n");
+
+proto.fragmentShader = [
+	"precision mediump float;",
+	"uniform vec3 mainCol;",
+	"varying vec2 mousePosF;",
+	"void main()",
+	"{",
+		"float mouseDist = length(mousePosF);",
+		"gl_FragColor = vec4(mainCol.r + mouseDist * 1., mainCol.g, mainCol.b, 1.0);",
 	"}"
 ].join("\n");
