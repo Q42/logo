0f0ac4b5747e54594cda520a589ce7bf75c7e9a6
Bob van Oorschot <bob@q42.nl>
2017-09-08 14:17:57 +0200

    Update logo.js

--- src/logo.js
+++ src/logo.js
@@ -33,7 +33,7 @@ function Q42Logo(element){
 
 	// Credits and a11y
 	element.setAttribute('title', 'Q42');
-	element.setAttribute('role', 'logo');
+	element.setAttribute('role', 'img');
 	element.setAttribute('aria-role', 'img');
 	element.setAttribute('aria-label', 'Q42 Logo');
 
