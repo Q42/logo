0104c684b88c0984c0dace211d8b7180dd489430
Jiri Swen <jiri.ei@hotmail.com>
2023-02-27 14:26:07 +0100

    WebGL formatting improvements

--- src/modules/webgl-flap.js
+++ src/modules/webgl-flap.js
@@ -10,50 +10,52 @@ proto.author = 'Sjoerd';
 
 proto.uniforms['amp'] = 1;
 
-proto.vertexShader = [
-	"attribute vec2 pos;",
-	"uniform float time;",
-	"uniform float amp;",
-	"uniform vec2 mousePos;",
-	"varying vec2 mousePosF;",
-	"varying float ampF;",
-	"varying float timeF;",
-	"varying float flap;",
-	"uniform vec2 ratio;",
-	"varying vec3 position;",
-	"void main()",
-	"{",
-		"float a = amp * 3.1415 * 2.;",
-		"float curve = (1. - cos(a)) * .5;",
-		"flap = sin(time / 100.) * pow(pos.x, 2.) * pow(amp, .3);",
-		"position = vec3(pos.x * cos(flap), pos.y, 1.0 - curve * (1. - cos(pos.y)) - flap * (1. - amp*.5));",
-		"position = vec3(position.x, position.y * cos(a) + position.z * sin(a), position.z * cos(a) - position.y * sin(a));",
-		"position = vec3(position.xy / ((2.5 - position.z) / 1.5) * ratio, 0.);",
-		"mousePosF = mousePos;",
-		"ampF = amp;",
-		"timeF = time;",
-		"gl_Position = vec4(position,1.0);",
-	"}"
-].join("\n");
+// todo: ga gwn dit proberen te runnen in een shader sandbox
 
-proto.fragmentShader = [
-	"precision mediump float;",
-	"uniform vec3 mainCol;",
-	"varying float ampF;",
-	"varying vec2 mousePosF;",
-	"varying float timeF;",
-	"varying vec3 position;",
-	"void main()",
-	"{",
-		"float mouseDist = length(mousePosF);",
-		"vec3 color = mainCol;",
-		"float lighten = sin(timeF / 100.) * pow(position.x, 1.) * pow(ampF, .3) * -1.;",
-		"color.r = 1. - (1. - mainCol.r) * (1. - ampF * lighten);",
-		"color.g = 1. - (1. - mainCol.g) * (1. - ampF * lighten);",
-		"color.b = 1. - (1. - mainCol.b) * (1. - ampF * lighten);",
-		"gl_FragColor = vec4(color , 1.0);",
-	"}"
-].join("\n");
+proto.vertexShader = glsl`
+	attribute vec2 pos;
+	uniform float time;
+	uniform float amp;
+	uniform vec2 mousePos;
+	varying vec2 mousePosF;
+	varying float ampF;
+	varying float timeF;
+	varying float flap;
+	uniform vec2 ratio;
+	varying vec3 position;
+
+	void main() {
+		float a = amp * 3.1415 * 2.;
+		float curve = (1. - cos(a)) * .5;
+		flap = sin(time / 100.) * pow(pos.x, 2.) * pow(amp, .3);
+		position = vec3(pos.x * cos(flap), pos.y, 1.0 - curve * (1. - cos(pos.y)) - flap * (1. - amp*.5));
+		position = vec3(position.x, position.y * cos(a) + position.z * sin(a), position.z * cos(a) - position.y * sin(a));
+		position = vec3(position.xy / ((2.5 - position.z) / 1.5) * ratio, 0.);
+		mousePosF = mousePos;
+		ampF = amp;
+		timeF = time;
+		gl_Position = vec4(position,1.0);
+	}
+`
+
+proto.fragmentShader = glsl`
+	precision mediump float;
+	uniform vec3 mainCol;
+	varying float ampF;
+	varying vec2 mousePosF;
+	varying float timeF;
+	varying vec3 position;
+	
+	void main() {
+		float mouseDist = length(mousePosF);
+		vec3 color = mainCol;
+		float lighten = sin(timeF / 100.) * pow(position.x, 1.) * pow(ampF, .3) * -1.;
+		color.r = 1. - (1. - mainCol.r) * (1. - ampF * lighten);
+		color.g = 1. - (1. - mainCol.g) * (1. - ampF * lighten);
+		color.b = 1. - (1. - mainCol.b) * (1. - ampF * lighten);
+		gl_FragColor = vec4(color , 1.0);
+	}
+`
 
 proto.initModule = function(){
 	this.logo.element.addEventListener('mouseenter', this.enter.bind(this));
--- src/modules/webgl-freak.js
+++ src/modules/webgl-freak.js
@@ -12,50 +12,48 @@ proto.margin = 100;
 
 proto.uniforms['amp'] = 1;
 
-proto.vertexShader = [
-	"attribute vec2 pos;",
-	"uniform float time;",
-	"uniform float scale;",
-	"uniform float amp;",
-	"uniform vec2 mousePos;",
-	"varying vec2 mousePosF;",
-	"varying float ampF;",
-	"uniform vec2 ratio;",
-	"varying vec3 position;",
-	"void main()",
-	"{",
-		"position = vec3(pos*(1.-amp),0.) + amp * vec3(pos.x*mousePos.x+pos.x*sin((pos.y*time)/200.),-mousePos.y*pos.y+pos.y*cos(pos.x*time/1000.),pos.y*cos(time/2000.));",
-		"position.xy *= ratio;",
-		"position.xy *= scale;",
-		"mousePosF = mousePos;",
-		"ampF = amp;",
-		"gl_Position = vec4(position,1.0);",
-	"}"
-].join("\n");
+proto.vertexShader = glsl`
+	attribute vec2 pos;
+	uniform float time;
+	uniform float scale;
+	uniform float amp;
+	uniform vec2 mousePos;
+	varying vec2 mousePosF;
+	varying float ampF;
+	uniform vec2 ratio;
+	varying vec3 position;
 
-proto.fragmentShader = [
-	"precision mediump float;",
-	"uniform vec3 mainCol;",
-	"varying float ampF;",
-	"varying vec2 mousePosF;",
-	"varying vec3 position;",
-	"const vec4 white = vec4(1.,1.,1.,1.);",
-	"const vec4 transparent = vec4(0.,0.,0.,0.);",
-	"void main()",
-	"{",
-		"float mouseDist = length(mousePosF);",
-		"vec4 color = vec4(mainCol,1.);",
-		"color.r = (1.-ampF) * mainCol.r + ampF * (mainCol.r + mouseDist * 1. + position.x);",
-		"color.g = (1.-ampF) * mainCol.g + ampF * (mainCol.g + position.z);",
-		"color.b = (1.-ampF) * mainCol.b + ampF * (mainCol.b + position.y);",
+	void main() {
+		position = vec3(pos*(1.-amp),0.) + amp * vec3(pos.x*mousePos.x+pos.x*sin((pos.y*time)/200.),-mousePos.y*pos.y+pos.y*cos(pos.x*time/1000.),pos.y*cos(time/2000.));
+		position.xy *= ratio;
+		position.xy *= scale;
+		mousePosF = mousePos;
+		ampF = amp;
+		gl_Position = vec4(position,1.0);
+	}
+`
 
-		"if(mainCol == white.rgb) {",
-				"color = (1.-ampF) * white + ampF * transparent;",
-		"}",
+proto.fragmentShader = glsl`
+	precision mediump float;
+	uniform vec3 mainCol;
+	varying float ampF;
+	varying vec2 mousePosF;
+	varying vec3 position;
+	const vec4 white = vec4(1.,1.,1.,1.);
+	const vec4 transparent = vec4(0.,0.,0.,0.);
 
-		"gl_FragColor = color;",
-	"}"
-].join("\n");
+	void main() {
+		float mouseDist = length(mousePosF);
+		vec4 color = vec4(mainCol,1.);
+		color.r = (1.-ampF) * mainCol.r + ampF * (mainCol.r + mouseDist * 1. + position.x);
+		color.g = (1.-ampF) * mainCol.g + ampF * (mainCol.g + position.z);
+		color.b = (1.-ampF) * mainCol.b + ampF * (mainCol.b + position.y);
+		if(mainCol == white.rgb) {
+		color = (1.-ampF) * white + ampF * transparent;
+		}
+		gl_FragColor = color;
+	}
+`
 
 proto.initModule = function(){
 	this.logo.element.addEventListener('mouseenter', this.enter.bind(this));
